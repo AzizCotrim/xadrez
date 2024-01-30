11e02d53efbf53c1b21d2a63d24d24bb866ae9d0
Aziz Cotrim <azizcotrim8@gmail.com>
2024-01-30 09:15:04 -0300

    fazendo os primeiros testes de movimentacao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,11 +6,27 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            Console.WriteLine(pos);
+                while (!partida.terminada) {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-            Console.WriteLine(pos.toPosicao());
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
+                Tela.imprimirTabuleiro(partida.tab);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
             Console.WriteLine();
         }
